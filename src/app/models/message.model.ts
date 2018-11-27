f062d2e19a9a017d61e4ac9a23afb89691e3db84
rscheurman <riley.scheurman@gmail.com>
2018-11-27 16:59:23 -0700

    added basic homepage massaging layout

---
+++ src/app/models/message.model.ts
@@ -0,0 +1,3 @@
+export class Message {
+	constructor(title: string, subtitle: string, body: string) {}
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Message } from 'src/app/models/message.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.scss' ]
 })
 export class HomeComponent implements OnInit {
+	homeMessages: Message[] = [
+		{
+			title: 'Welcome back!',
+			subtitle: 'Start recording your jumps!',
+			body: 'This is the body'
+		},
+		{
+			title: 'Welcome back!',
+			subtitle: 'Start recording your jumps!',
+			body: 'This is the body'
+		},
+		{
+			title: 'Welcome back!',
+			subtitle: 'Start recording your jumps!',
+			body: 'This is the body'
+		}
+	];
+	constructor(private fbAuth: AuthService) {}
 
-  constructor(private fbAuth: AuthService) { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
