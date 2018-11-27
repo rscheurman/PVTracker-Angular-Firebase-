e411543ba6450fd843f913af3e31d8950a98ddc5
rscheurman <riley.scheurman@gmail.com>
2018-11-26 21:26:44 -0700

    - implemented guest user nav template for updating user navigation with provided name.

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -4,13 +4,14 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 	user: Observable<firebase.User>;
 	authenticated: boolean = false;
 
 	name: string;
-	uid: string;
 	email: string;
+	uid: string;
 
 	constructor(private afa: AngularFireAuth, public afs: AngularFirestore) {
 		this.afa.authState.subscribe((auth) => {
@@ -18,15 +19,12 @@ export class AuthService {
 				this.user = afa.authState;
 				this.authenticated = true;
 
-				// Get User Info
 				this.user.subscribe((user: firebase.User) => {
-					this.name = user.displayName;
-					this.uid = user.uid;
-					this.email = user.email;
+					this.addUserData(user.displayName, user.email, user.uid);
 				});
 			} else {
+				this.user = null;
 				this.authenticated = false;
-				this.name = 'Guest';
 			}
 		});
 	}
@@ -39,5 +37,14 @@ export class AuthService {
 	logout() {
 		this.afa.auth.signOut();
 		this.authenticated = false;
+	}
+
+	addUserData(name: string, email: string, uid: string) {
+		this.afs.collection('users').doc(uid).set({
+			name: name,
+			email: email,
+			uid: uid
+		});
+		console.log(name + " user's data was added");
 	}
 }
